0baf3ac600676cd457e565714c55488f6988882c
shira1008 <shaharshira1008@gmail.com>
2023-09-12 14:34:56 +0300

    added more projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,8 +4,8 @@ const projectsList = [
   {
     id: 1,
     title: "Portfolio",
-    subtitle: "Website",
-    description: "Portfolio to my dog while learning css",
+    subtitle: "CSS, HTML, JavaScript",
+    description: "Portfolio to my dog while learning css grid",
     img: "./assets/img/Screenshot_1.jpg",
     git: "https://github.com/shira1008/grid-porfolio-project",
     liveSite: "https://shish-profile.netlify.app/",
@@ -14,48 +14,64 @@ const projectsList = [
   {
     id: 2,
     title: "Wikipedia Api",
-    subtitle: "Website",
-    description: "Made Wikipedia",
+    subtitle: "JavaScript, CSS, HTML",
+    description:
+      "I used wikipedia API to create a website that looks similar to wikipedia and display the searching results.",
     img: "./assets/img/wikipediat_2.jpg",
     git: "https://github.com/shira1008/Wikipedia-API",
     liveSite: "https://wikipedia-shish.netlify.app/",
   },
-  {
-    id: 3,
-    title: "Portfolio",
-    subtitle: "Website",
-    description: "Portfolio to my dog while learning css",
-    img: "./assets/img/Screenshot_1.jpg",
-    git: "https://github.com/shira1008/grid-porfolio-project",
-    liveSite: "https://shish-profile.netlify.app/",
-  },
+  // {
+  //   id: 3,
+  //   title: "Clothes Store",
+  //   subtitle: "SCSS, HTML",
+  //   description: "Learning SCSS from a Udemy course , only SCSS and HTML.",
+  //   img: "./assets/img/scsst_3.jpg",
+  //   git: "https://github.com/shira1008/Clothes-Store",
+  //   liveSite: "https://scss-project-shish.netlify.app/",
+  // },
   {
     id: 4,
-    title: "Portfolio",
-    subtitle: "Website",
-    description: "Portfolio to my dog while learning css",
-    img: "./assets/img/Screenshot_1.jpg",
-    git: "https://github.com/shira1008/grid-porfolio-project",
-    liveSite: "https://shish-profile.netlify.app/",
+    title: "Furniture Store",
+    subtitle: "JavaScript, HTML, CSS",
+    description:
+      "A furniture store. I used an API from a Udemy course( JS Course ), Created the functionality using JS. ",
+    img: "./assets/img/comfy.jpg",
+    git: "https://github.com/shira1008/Store-Project",
+    liveSite: "https://store-shira.netlify.app/",
   },
 
   {
     id: 5,
-    title: "Wikipedia Api",
-    subtitle: "Website",
-    description: "Made Wikipedia",
-    img: "./assets/img/wikipediat_2.jpg",
+    title: "GitHub Api",
+    subtitle: "React, HTML, CSS",
+    description:
+      "React project, using GitHub API and display data about searched user. the user have to login.",
+    img: "./assets/img/github.jpg",
     git: "https://github.com/shira1008/Wikipedia-API",
-    liveSite: "https://wikipedia-shish.netlify.app/",
+    liveSite: "https://github-api-project-shira.netlify.app//",
   },
   {
     id: 6,
-    title: "Portfolio",
-    subtitle: "Website",
-    description: "Portfolio to my dog while learning css",
-    img: "./assets/img/Screenshot_1.jpg",
-    git: "https://github.com/shira1008/grid-porfolio-project",
-    liveSite: "https://shish-profile.netlify.app/",
+    title: "Movie Web",
+    subtitle: "Python - Flask, CSS, HTML, sqlalchemy, sqlite",
+    description:
+      "The MoviWeb App is a Flask-based, Fullstack application that allows users to track their favorite movies. ",
+    img: "./assets/img/MovieWebt.jpg",
+    git: "https://github.com/shira1008/MoviWeb/tree/main",
+    liveSite: "http://shirashahar.pythonanywhere.com/",
+  },
+
+  {
+    id: 7,
+    title: "Recipe Keeper",
+    subtitle: "Python - fastapi, JavaScript, HTML, CSS",
+    description:
+      "The Recipe Keeper App is a web-based solution designed to simplify recipe management. The live site, showcases the frontend-only version (its look the same)",
+
+    img: "./assets/img/recipe.jpg",
+    git: "https://github.com/shira1008/Recipe-keeper-with-backend",
+    liveSite: "https://recipe-keeper-shira.netlify.app/",
   },
 ];
 
